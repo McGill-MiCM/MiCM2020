f66ad44aeae516cd65a89380e07f464766bb1d5a
Yi Lian <yi.lian@mail.mcgill.ca>
2020-03-03 10:39:11 -0500

    update 20200303

--- Efficiency_LY.r
+++ Efficiency_LY.r
@@ -1,17 +1,16 @@
-
 # double
 class(5); is.double(5)
 
 # integer
 class(5L); is.double(5L)
 
-object.size(rep(5, 1000))
-object.size(rep(5L, 1000))
-
 # How precise is double precision?
 options(digits = 22) # show more digits in output
 print(1/3)
-options(digits = 7) # default
+options(digits = 7) # back to the default
+
+object.size(rep(5, 10))
+object.size(rep(5L, 10))
 
 # logical
 class(TRUE); class(F)
@@ -25,6 +24,7 @@ fac
 
 class(fac)
 
+# R has an algorithm to decide the order of the levels
 fac.ch <- as.factor(c("B", "a", "1", "ab", "b", "A"))
 fac.ch
 
@@ -46,6 +46,8 @@ mymatrix
 
 class(mymatrix)
 
+str(mymatrix)
+
 # Array - not important for this workshop
 myarray <- array(c(1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144), dim = c(2, 2, 3))
 print(myarray) # print() is not needed if run in R or Rstudio.
@@ -53,11 +55,11 @@ print(myarray) # print() is not needed if run in R or Rstudio.
 class(myarray)
 
 # List - very important for the workshop
-mylist <- list(Title = "Efficient Coding and Computing",
-               Duration = c(3, 3),
+mylist <- list(Title = "R Beyond the Basics",
+               Duration = c(2, 2),
                sections = as.factor(c(1, 2, 3, 4)),
-               Date = as.Date("2019-08-13"),
-               Lunch_provided = TRUE,
+               Date = as.Date("2020-03-06"),
+               Lunch_provided = FALSE,
                Feedbacks = c("Amazing!", "Great workshop!", "Yi is the best!", "Wow!")
 )
 print(mylist) # No need for print if running in R or Rstudio
@@ -86,19 +88,12 @@ head(mtcars)
 # Access a column (variable) in data frames
 mtcars$mpg
 
-# Let's try to invert a large matrix.
-A <- diag(4000)
-# A.inv <- solve(A)
-
-# optim() in R calls C programs, run optim to see source code.
-# optim
-
 # Vectorized operation
+# system.time(operation)  returns the time needed to run the 'operation'
 t <- system.time( x1 <- sqrt(1:1000000) )
 head(x1)
 
-# We can do worse
-# For loop with memory pre-allocation
+# For loop
 x2 <- rep(NA, 1000000)
 t0 <- proc.time()
 for (i in 1:1000000) {
@@ -108,31 +103,13 @@ t1 <- proc.time()
 
 identical(x1, x2) # Check whether results are the same
 
-# Even worse
-# For loop without memory pre-allocation
-x3 <- NULL
-t2 <- proc.time()
-for (i in 1:1000000) {
-    x3[i] <- sqrt(i)
-}
-t3 <- proc.time()
-
-identical(x2, x3) # Check whether results are the same
-
-# As we can see, R is not very good with loops.
-t; t1 - t0; t3 - t2
+# As we can see, R is not very fast with loops.
+t; t1 - t0
 # ?proc.time
 
 # microbenchmark runs the code multiple times and take a summary
+# Use well-developped R function
 library(microbenchmark)
-result <- microbenchmark(sqrt(1:1000000),
-                         for (i in 1:1000000) {x2[i] <- sqrt(i)},
-                         unit = "s", times = 20
-                        )
-summary(result)
-# Result in seconds
-
-# Use well-developped R functions
 result <- microbenchmark(sqrt(500),
                          500^0.5,
                          unit = "ns", times = 1000
@@ -140,93 +117,60 @@ result <- microbenchmark(sqrt(500),
 summary(result)
 # Result in nanoseconds
 
-data <- read.csv("https://raw.githubusercontent.com/ly129/MiCM/master/sample.csv", header = TRUE)
-head(data, 10)
+data <- read.csv("https://raw.githubusercontent.com/ly129/MiCM2020/master/sample.csv", header = TRUE)
+head(data, 8)
 
 summary(data)
 
-mean(data$Wr.Hnd)
 
-mean(data$Height)
-?mean
 
-mean(data$Height, na.rm = TRUE)
 
-cts.var <- sapply(X = data, FUN = is.double) # We'll talk about sapply later.
-cts <- data[ , cts.var]
-head(cts)
-?apply
 
-apply(X = cts, MARGIN = 2, FUN = mean)
 
-apply(X = cts, MARGIN = 2, FUN = mean, na.rm = T)
 
-fm <- table(data$Sex)
-fm
+# Choose the continuous variables
 
-class(fm)
+# Calculate the mean
 
-fm/length(data$Sex)
 
-prop.table(fm)
 
-table(data$Smoke)
 
-table(data$Smoke, data$Sex)
 
-table(data[, c("Smoke", "Sex")])
 
-table <- aggregate(x = data$Wr.Hnd, by = list(Sex = data$Sex), FUN = sd)
-table
-# table[table$Sex == "Female",]
 
-aggregate(Wr.Hnd~Sex, FUN = sd, data = data)
 
-by(data = data$Wr.Hnd, INDICES = list(Sex = data$Sex), FUN = sd)
 
-table1 <- tapply(X = data$Wr.Hnd,
-                 INDEX = list(Sex = data$Sex),
-                 FUN = sd,
-                 simplify = T)
-# tapply(X = data$Wr.Hnd, INDEX = list(Sex = data$Sex), FUN = sd)["Female"]
-table1
-str(table1)
+# aggregate() syntax 1
+
+# aggregate() syntax 2
+
+# by()
+
+# tapply()
 
 # Return a list using tapply()
-table2 <- tapply(X = data$Wr.Hnd,
-                 INDEX = list(Sex = data$Sex),
-                 FUN = sd,
-                 simplify = F)
-table2
-str(table2)
 
-aggregate(x = data$Wr.Hnd,
-          by = list(Sex = data$Sex, Smoke = data$Smoke),
-          FUN = sd)
 
-aggregate(Wr.Hnd~Sex + Smoke, data = data, FUN = sd)
 
-aggregate(cbind(Wr.Hnd, NW.Hnd) ~ Sex + Smoke, data = data, FUN = sd)
 
-name <- aggregate(x = cbind(data$Wr.Hnd, data$NW.Hnd),
-          by = list(Sex = data$Sex, Smoke = data$Smoke),
-          FUN = sd)
-name
 
-aggregate(Wr.Hnd~Sex+Smoke, data = data, FUN = print)
 
-aggregate(Wr.Hnd~Sex+Smoke, data = data, FUN = length)
 
-aggregate(Wr.Hnd~Sex+Smoke, data = data, FUN = hist)
+
+
+
+
+
+
+
+
 
 vec <- 1:5
 vec
 
 ifelse(vec>3, yes = "big", no = "small")
 
-adult <- 18
-data$Adult <- ifelse(data$Age>=18, "Yes", "No")
-head(data)
+
 
 if (data$Age >= 18) {
     data$Adult2 = "Yes"
@@ -239,67 +183,46 @@ head(data)
 data <- subset(data, select=-c(Adult2))
 
 cut.points <- c(0, 16, 18, 20, 22, Inf)
-data$Hnd.group <- cut(data$Wr.Hnd, breaks = cut.points, right = TRUE)
+
 head(data)
 # labels as default
 
 # Set labels to false
-data$Hnd.group <- cut(data$Wr.Hnd,
-                      breaks = cut.points,
-                      labels = F, right = TRUE)
-head(data)
+
 
 # Customized labels
-label <- c("Curry", "Drake", "VanVleet", "Lin", "Leonard")
-data$Hnd.group <- cut(data$Wr.Hnd,
-                      breaks = cut.points,
-                      labels = label, right = TRUE)
-head(data)
+label <- c("TP/XS", "P/S", "M/M", "G/L", "TG/XL")
 
-aggregate(Wr.Hnd~Hnd.group, data = data, FUN = mean)
+
+
 
 # cut.points <- c(0, 16, 18, 20, 22, Inf)
-Wr.Hnd.Grp <- split(data$Wr.Hnd, f = data$Hnd.group)
-Wr.Hnd.Grp
+
 
 # lapply
-la <- lapply(Wr.Hnd.Grp, FUN = summary)
-la
-class(la)
+
 
 # sapply
-sa <- sapply(X = Wr.Hnd.Grp, FUN = summary, simplify = T)
-sa
-class(sa)
-# See what simplify does
 
-sa <- sapply(X = Wr.Hnd.Grp, FUN = summary, simplify = F)
-sa
-class(sa)
+
+
 
 # vapply *
 # Safer than sapply(), and a little bit faster
 # because FUN.VALUE has to be specified that length and type should match
-# Any idea why it can be a little bit faster? Recall...
-va <- vapply(Wr.Hnd.Grp, summary, FUN.VALUE = c("Min." = numeric(1),
-                                                "1st Qu." = numeric(1),
-                                                "Median" = numeric(1),
-                                                "Mean" = numeric(1),
-                                                "3rd Qu." = numeric(1),
-                                                "Max." = numeric(1)))
-va
+
+# va <- vapply(Wr.Hnd.Grp, summary, FUN.VALUE = c("Min." = numeric(1),
+#                                                 "1st Qu." = numeric(1),
+#                                                 "Median" = numeric(1),
+#                                                 "Mean" = numeric(1),
+#                                                 "3rd Qu." = numeric(1),
+#                                                 "Max." = numeric(1)))
+# va
 
 # aggregate(Wr.Hnd~Smoke, data = data, FUN = ...)
 # tapply(X = data$Wr.Hnd, INDEX = list(data$Smoke), FUN = ...)
 
-sample.mean <- aggregate(Wr.Hnd~Smoke, data = data, FUN = mean)$Wr.Hnd
-sample.sd <- aggregate(Wr.Hnd~Smoke, data = data, FUN = sd)$Wr.Hnd
-n <- aggregate(Wr.Hnd~Smoke, data = data, FUN = length)$Wr.Hnd
-t <- qt(p = 0.025, df = n - 1, lower.tail = FALSE)
-sample.mean; sample.sd; n; t
-lb <- sample.mean - t * sample.sd / sqrt(n)
-ub <- sample.mean + t * sample.sd / sqrt(n)
-lb; ub
+
 # How many times did we aggregate according to the group? Can on aggregate only once?
 
 # The structure
@@ -315,15 +238,15 @@ times2 <- function(x) {
 # Use the function
 times2(x = 5)
 # or
-times2(5)
+times2(3)
 
 # R has operators that do this
 9 %/% 2
 9 %% 2
 
 int.div <- function(a, b){
-    int <- a%/%b
-    mod <- a%%b
+    int <- floor(a/b)
+    mod <- a - int*b
     return(list(integer = int, modulus = mod))
 }
 
@@ -351,7 +274,7 @@ int.div(21, 4)
 AIcanadian <- function(who, reply_to) {
     system(paste("say -v", who, "Sorry!"))
 }
-AIcanadian("Alex", "Sorry I stepped on your foot.")
+# AIcanadian("Alex", "Sorry I stepped on your foot.")
 
 # Train my chatbot - AlphaGo style.
 # I'll let Alex and Victoria talk to each other.
@@ -371,214 +294,52 @@ chat_log <- rep(NA, 8)
 # chat_log
 
 data_summary <- function(func) {
-    data <- read.csv("https://raw.githubusercontent.com/ly129/MiCM/master/sample.csv", header = TRUE)
+    data <- read.csv("https://raw.githubusercontent.com/ly129/MiCM2020/master/sample.csv", header = TRUE)
     by(data = data$Wr.Hnd, INDICES = list(data$Smoke), FUN = func)
 }
 data_summary(mean)
 
-# sample.mean <- NULL
-# sample.sd <- NULL
-# n <- NULL
-# t <- qt(p = 0.025, df = n - 1, lower.tail = FALSE)
-# lb <- sample.mean - t * sample.sd / sqrt(n)
-# ub <- sample.mean + t * sample.sd / sqrt(n)
-
-sample_CI <- function(x) {
-    m <- mean(x)
-    l <- length(x)
-    s <- sd(x)
-    t <- qt(p = .025, df = l - 1, lower.tail = FALSE)
-    lb <- m - t* s / sqrt(l)
-    ub <- m + t * s / sqrt(l)
-    return(c(LowerBound = lb, UpperBound = ub))
-}
-
-aggregate(Wr.Hnd~Smoke, data = data, FUN = sample_CI)
-
-library(parallel)
-detectCores()
-#Multi-core computing not supported on Windows :(
-
-mat.list <- sapply(c(1, 5, 200, 250, 1800, 2000), diag)
-print(head(mat.list, 2)) # print() makes the output here look the same as in R/Rstudio
-
-system.time(
-    sc <- lapply(mat.list, solve)
-)
-
-system.time(
-    mc <- mclapply(mat.list, solve, mc.preschedule = TRUE, mc.cores = 3)
-)
-
-system.time(
-    mc <- mclapply(mat.list, solve, mc.preschedule = FALSE, mc.cores = 3)
-)
-
-t <- proc.time()
-cl <- makeCluster(3) # Use 3 cores
-pl <- parLapply(cl = cl, X = mat.list, fun = solve)
-stopCluster(cl)
-proc.time() - t
-
-t <- proc.time()
-cl <- makeCluster(3)
-pl <- parLapplyLB(cl = cl, X = mat.list, fun = solve)
-stopCluster(cl)
-proc.time() - t
-
-# Two parallel calls within one cluster.
-t <- proc.time()
-cl <- makeCluster(3)
-pl_nb <- parLapply(cl = cl, X = mat.list, fun = solve)
-pl_lb <- parLapplyLB(cl = cl, X = mat.list, fun = solve)
-stopCluster(cl)
-proc.time() - t
-# This takes shorter than the sum of the previous two. Why?
-
-t <- proc.time()
-cl <- makeCluster(3)
-stopCluster(cl)
-proc.time() - t
-
-library(Rcpp)
-sourceCpp("sqrt_cpp.cpp")
-square_root(1:4)
-# We return a NumericVector in the .cpp file. So we get an R vector.
-
-sourceCpp("mm_cpp.cpp")
-
-# Now we can call the function using the name defined in the .cpp file
-set.seed(20190813)
-a <- matrix(rnorm(100000), ncol = 50000)  # 2 x 50000 matrix
-b <- matrix(rnorm(200000), nrow = 50000)  # 50000 x 4 matrix
-
-mat_mul(a, b)
-# We return an Rcpp::List in the .cpp file. So we get an R list here.
-# mat_mul(b, a)
-
-bchmk <- microbenchmark(a %*% b,
-                        mat_mul(a, b),
-                        unit = "us", times = 100
-                       )
-summary(bchmk)
-
-# Here we make an R function that calls the C++ function
-mmc <- function(a, b) {
-    result <- mat_mul(a, b)$MatrixMultiplication
-    return(result)
-}
-mmc(a, b)
-
-# Another way to do this. Here you do not need to have a separate .cpp file.
-# A naive .cpp function is made here.
-library(RcppArmadillo)
-cppFunction(depends = "RcppArmadillo",
-            code = 'arma::mat mm(arma::mat& A, arma::mat& B){
-                        return A * B;
-                    }'
-)
-
-mm(a, b)
-# mm(b, a)
-
-# We can wrap this naive function in an R function to manipulate input and output in R
-mmc2 <- function(A, B) {
-    if (ncol(A) == nrow(B)) {
-        return(mm(A, B))
-    } else {
-        stop("non-conformable arguments")
+a_times_2_unless_you_want.something.else.but.I.refuse.3 <- function(a, b=2){
+    if (b == 3) {
+        stop("I refuse 3!")
     }
-}
-mmc2(a, b)
-# mmc2(b, a)
-
-set.seed(20190813)
-
-ra <- 2
-ca <- 4
-rb <- 4
-cb <- 3
-
-A <- matrix(rnorm(ra*ca), nrow = ra)
-B <- matrix(rnorm(rb*cb), nrow = rb)
-
-A; B
-
-# Load the executable .so file (MacOS) or .dll file (Windows)
-dyn.load("mm_for.so")
-
-# Check whether the "mat_mul_for" function is loaded into R
-is.loaded("mat_mul_for")
-
-result <- .Fortran("mat_mul_for",
-                   A = as.double(A),
-                   B = as.double(B),
-                   AB = double(ra * cb),  # note the difference here
-                   RowA = as.integer(ra),
-                   ColA = as.integer(ca),
-                   RowB = as.integer(rb),
-                   ColB = as.integer(cb),
-                   RowAB = as.integer(ra),
-                   ColAB = as.integer(cb)
-)
-result
-class(result)
-
-mmf <- function(A, B) {
-    ra <- nrow(A)
-    ca <- ncol(A)
-    rb <- nrow(B)
-    cb <- ncol(B)
-    
-    if (ca == rb) {
-        result <- .Fortran("mat_mul_for",
-                            A = as.double(A),
-                            B = as.double(B),
-                            AB = double(ra * cb),
-                            RowA = as.integer(ra),
-                            ColA = as.integer(ca),
-                            RowB = as.integer(rb),
-                            ColB = as.integer(cb),
-                            RowAB = as.integer(ra),
-                            ColAB = as.integer(cb)
-                            )
-        mm <- matrix(result$AB, nrow = result$RowAB, byrow = F)
-    } else {
-        stop('non-conformable arguments')
-    }
-    return(list(Result = mm,
-                Dimension = c(result$RowAB, result$ColAB)
-               )
-          )
+    a*b
 }
 
-set.seed(20190813)
+a_times_2_unless_you_want.something.else.but.I.refuse.3(a = 5)
 
-ra <- 2
-ca <- 50000
-rb <- 50000
-cb <- 3
+a_times_2_unless_you_want.something.else.but.I.refuse.3(a = 5, b = 4)
 
-A <- matrix(rnorm(ra*ca), nrow = ra)
-B <- matrix(rnorm(rb*cb), nrow = rb)
+# a_times_2_unless_you_want.something.else.but.I.refuse.3(a = 5, b = 3)
 
-mmf(A, B)
 
-A %*% B
 
-# Something like this.
-9 %/% 2; 9%%2
 
-# Something like this.
-c(15, 14, 13, 12) %/% c(6, 5, 4, 3)
-c(15, 14, 13, 12) %% c(6, 5, 4, 3)
 
-# If you enter the right X and Y in your function, you should get the following result
-lm(Wr.Hnd~NW.Hnd+Age, data = data)
 
-# Something like this, both inputs are R functions.
-GD <- function(objective_function, gradient_function, initial_value) {
-    statements
-}
+
+set.seed(20200306)
+N <- 200
+height <- round(rnorm(n = N, mean = 180, sd = 10)) # in centimeter
+weight <- round(rnorm(n = N, mean = 80, sd = 10)) # in kilograms
+age <- round(rnorm(n = N, mean = 50, sd = 10))
+treatment <- sample(c(TRUE, FALSE), size = N, replace = T, prob = c(0.3,0.7))
+HF <- sample(c(TRUE, FALSE), size = N, replace = T, prob = c(0.1,0.9))
+
+fake <- data.frame(height, weight, age, treatment, HF)
+head(fake)
+
+
+
+
+
+# Trick:
+FALSE+TRUE+TRUE
+
+
+
+
+
+
 
 
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # MiCM - McGill initiative in Computational Medicine
-## Workshop: R programming beyong the basics
+## Workshop: R - Beyong the Basics
 Efficient coding and computing
 ## Yi Lian
 Department of Epidemiology, Biostatistics and Occupational Health, McGill University
-## 2019-08-13
+## 2020-03-06
 
 ### Workshop Materials
 #### Notes
@@ -12,20 +12,7 @@ Department of Epidemiology, Biostatistics and Occupational Health, McGill Univer
 
 #### R codes and data
 1. [Workshop code](Efficiency_LY.r)
-2. [R call Fortran Windows](RcallFor_windows.R)
-3. [Teaching data](sample.csv)
-
-#### C++ and Fortran codes
-1. [Square root: sqrt_cpp.cpp](sqrt_cpp.cpp)
-2. [Matrix multiplication: mm_cpp.cpp](mm_cpp.cpp)
-3. [Matrix multiplication: mm_for.f90](mm_for.f90)
-
-#### Miscellaneous
-1. [Fortran setup on Windows](Fortran_Setup_Win.txt)
-
+2. [Teaching data](sample.csv)
 
 ### Useful Links
 1. [R](https://www.r-project.org)
-2. [MacOS compilers](https://cran.r-project.org/bin/macosx/tools/)
-3. [Armadillo](http://arma.sourceforge.net)
-  - [Armadillo Linear Algebra](https://gitlab.com/conradsnicta/armadillo-code/blob/9.600.x/armadillo_joss_2016.pdf)
