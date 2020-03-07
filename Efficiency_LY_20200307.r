1e3c721fb31156cdf5d3c1b1f97062d3734fcf1d
Yi Lian <yi.lian@mail.mcgill.ca>
2020-03-07 14:56:33 -0500

    After March 07

--- Efficiency_LY_20200307.r
+++ Efficiency_LY_20200307.r
@@ -1,6 +1,11 @@
 # double
 class(5); is.double(5)
 
+class(5.0);
+is.double(5.0)
+
+5L/2L
+
 # integer
 class(5L); is.double(5L)
 
@@ -9,8 +14,8 @@ options(digits = 22) # show more digits in output
 print(1/3)
 options(digits = 7) # back to the default
 
-object.size(rep(5, 10))
-object.size(rep(5L, 10))
+object.size(rep(5, 5e6))
+object.size(rep(5L, 5e6))
 
 # logical
 class(TRUE); class(F)
@@ -93,7 +98,7 @@ mtcars$mpg
 t <- system.time( x1 <- sqrt(1:1000000) )
 head(x1)
 
-# For loop
+# For loop with memory pre-allocation
 x2 <- rep(NA, 1000000)
 t0 <- proc.time()
 for (i in 1:1000000) {
@@ -103,8 +108,18 @@ t1 <- proc.time()
 
 identical(x1, x2) # Check whether results are the same
 
+# For loop without memory pre-allocation
+x3 <- NULL
+t2 <- proc.time()
+for (i in 1:1000000) {
+    x3[i] <- sqrt(i)
+}
+t3 <- proc.time()
+
+identical(x1, x2) # Check whether results are the sa
+
 # As we can see, R is not very fast with loops.
-t; t1 - t0
+t; t1 - t0; t3 - t2
 # ?proc.time
 
 # microbenchmark runs the code multiple times and take a summary
@@ -122,55 +137,97 @@ head(data, 8)
 
 summary(data)
 
+mean(data$Wr.Hnd)
 
+mean(data$Height)
 
-
-
-
+mean(data$Height, na.rm = TRUE)
 
 # Choose the continuous variables
+var.cts <- sapply(data, FUN = is.numeric)
+var.cts <- c("Wr.Hnd", "NW.Hnd", "Pulse", "Height", "Age")
+
+cts.data <- data[, var.cts]
+head(cts.data)
 
 # Calculate the mean
+apply(cts.data, MARGIN = 2, FUN = mean)
 
+apply(cts.data, MARGIN = 2, FUN = mean, na.rm = TRUE)
 
+sex.tab <- table(data$Sex)
+sex.tab
 
+prop.table(sex.tab)
 
+table(data$Smoke)
 
+table(data$Sex, data$Smoke)
 
+ss.tab <- table(data[, c("Sex", "Smoke")])
+ss.tab
 
+prop.table(ss.tab, margin = 2)
 
+prop.table(ss.tab, margin = 1)
+
+sample(letters[1:3], size = 100, replace = T)
+
+tab3d <- table(data$Sex, data$Smoke,
+               sample(letters[1:3], size = 100, replace = T))
+tab3d
+
+prop.table(tab3d, margin = c(1,3))
+
+# Subset if we don't know these functions yet..
+data.f <- data[data$Sex == "Female", ]
+head(data.f)
+sd(data.f$Wr.Hnd)
 
 # aggregate() syntax 1
+aggregate(data$Wr.Hnd, by = list(sex = data$Sex), FUN = sd)
 
 # aggregate() syntax 2
+aggregate(Wr.Hnd~Sex, data = data, FUN = sd)
 
 # by()
+by(data = data$Wr.Hnd, INDICES = list(sex = data$Sex), FUN = sd)
 
 # tapply()
+tapply(X = data$Wr.Hnd, INDEX = list(data$Sex), FUN = sd)
 
 # Return a list using tapply()
+tapply(X = data$Wr.Hnd, INDEX = list(data$Sex),
+       FUN = sd, simplify = FALSE)
 
+aggregate(data$Wr.Hnd,
+          by = list(sex = data$Sex, smoke = data$Smoke),
+          FUN = sd)
 
+aggregate(Wr.Hnd~Sex+Smoke, data = data, FUN = sd)
 
+cbind(1:5, 5:1); rbind(1:5, 5:1)
 
+aggregate(cbind(data$Wr.Hnd, data$NW.Hnd),
+          by = list(data$Sex, data$Smoke),
+          FUN = sd)
 
+aggregate(cbind(Wr.Hnd, NW.Hnd)~Sex+Smoke, data = data, FUN = sd)
 
+aggregate(Wr.Hnd~Smoke, data = data, FUN = print)
 
+aggregate(Wr.Hnd~Smoke, data = data, FUN = length)
 
-
-
-
-
-
-
-
+par(mfrow = c(2,2))
+aggregate(Wr.Hnd~Smoke, data = data, FUN = hist,
+          main = "hist", breaks = 3)
 
 vec <- 1:5
 vec
 
 ifelse(vec>3, yes = "big", no = "small")
 
-
+data$Adult <- ifelse(data$Age >= 18, yes = "Yes", no = "No")
 
 if (data$Age >= 18) {
     data$Adult2 = "Yes"
@@ -182,48 +239,74 @@ head(data)
 # Delete Adult2
 data <- subset(data, select=-c(Adult2))
 
-cut.points <- c(0, 16, 18, 20, 22, Inf)
+cut.points <- c(-Inf, 16, 18, 20, 22, Inf)
 
-head(data)
+data$Hn.Grp <- cut(data$Wr.Hnd, breaks = cut.points, right = T)
+
+head(data, 12)
 # labels as default
 
 # Set labels to false
-
+data$Hn.Grp <- cut(data$Wr.Hnd, breaks = cut.points, labels = F)
+head(data)
 
 # Customized labels
-label <- c("TP/XS", "P/S", "M/M", "G/L", "TG/XL")
+custom.label <- c("TP/XS", "P/S", "M/M", "G/L", "TG/XL")
+data$Hn.Grp <- cut(data$Wr.Hnd, breaks = cut.points,
+                   right = T, labels = custom.label)
+head(data)
 
+aggregate(Wr.Hnd~Hn.Grp, data = data, FUN = mean)
 
+numbers <- 1:10
+groups <- sample(letters[1:3], size = 10, replace = T)
+rbind(numbers, groups)
 
+split(x = numbers, f = groups)
 
-# cut.points <- c(0, 16, 18, 20, 22, Inf)
-
+wr.hnd.grp <- split(data$Wr.Hnd, f = data$Hn.Grp)
+wr.hnd.grp
 
 # lapply
-
+la <- lapply(X = wr.hnd.grp, FUN = mean);
+la
 
 # sapply
+sapply(X = wr.hnd.grp, FUN = mean, simplify = T)
 
+sapply(X = wr.hnd.grp, FUN = mean, simplify = F)
 
-
+summary(1:10)
 
 # vapply *
 # Safer than sapply(), and a little bit faster
 # because FUN.VALUE has to be specified that length and type should match
 
-# va <- vapply(Wr.Hnd.Grp, summary, FUN.VALUE = c("Min." = numeric(1),
-#                                                 "1st Qu." = numeric(1),
-#                                                 "Median" = numeric(1),
-#                                                 "Mean" = numeric(1),
-#                                                 "3rd Qu." = numeric(1),
-#                                                 "Max." = numeric(1)))
-# va
+va <- vapply(wr.hnd.grp, summary, FUN.VALUE = c("Min." = numeric(1),
+                                                "1st Qu." = numeric(1),
+                                                "Median" = numeric(1),
+                                                "Mean" = numeric(1),
+                                                "3rd Qu." = numeric(1),
+                                                "Max." = numeric(1)))
+va
 
 # aggregate(Wr.Hnd~Smoke, data = data, FUN = ...)
 # tapply(X = data$Wr.Hnd, INDEX = list(data$Smoke), FUN = ...)
 
+sample.mean <- aggregate(Wr.Hnd~Smoke, data = data, FUN = mean)[,2]
+sample.var <- aggregate(Wr.Hnd~Smoke, data = data, FUN = var)[,2]
+sample.size <- aggregate(Wr.Hnd~Smoke, data = data, FUN = length)[,2]
 
-# How many times did we aggregate according to the group? Can on aggregate only once?
+# sample.mean; sample.var; sample.size
+
+t <- qt(p = 0.025, df = sample.size - 1, lower.tail = FALSE)
+
+lb <- sample.mean - t * sqrt(sample.var/sample.size); lb
+ub <- sample.mean + t * sqrt(sample.var/sample.size); ub
+
+
+# How many times did we aggregate according to the group?
+# Can we aggregate only once?
 
 # The structure
 func_name <- function(argument){
@@ -232,7 +315,7 @@ func_name <- function(argument){
 
 # Build the function
 times2 <- function(x) {
-    fx = 2 * x
+    fx <- 2 * x
     return(fx)
 }
 # Use the function
@@ -253,21 +336,22 @@ int.div <- function(a, b){
 # class(result)
 # Recall: how do we access the modulus?
 result <- int.div(21, 4)
-result$integer
+result
 
 int.div <- function(a, b){
     int <- a%/%b
     mod <- a%%b
     return(cat(a, "%%", b, ": \n integer =", int,"\n ------------------", " \n modulus =", mod, "\n"))
 }
-int.div(21,4)
+int.div(33,5)
 
 int.div <- function(a, b){
     int <- a%/%b
     mod <- a%%b
-    return(c(a, b))
+    out <- rbind(a, b, int, mod)
+    out
 }
-int.div(21, 4)
+int.div(21:25, 1:5)
 
 # No need to worry about the details here.
 # Just want to show that functions do not always have to return() something.
@@ -315,34 +399,145 @@ a_times_2_unless_you_want.something.else.but.I.refuse.3(a = 5)
 
 a_times_2_unless_you_want.something.else.but.I.refuse.3(a = 5, b = 4)
 
-# a_times_2_unless_you_want.something.else.but.I.refuse.3(a = 5, b = 3)
+a_times_2_unless_you_want.something.else.but.I.refuse.3(a = 5, b = 3)
 
+# Multiple optional arguments fed to different functions called in our own function
+# Still needs a lot of refinement
 
+fancy.mean <- function(vec, ...) {
+    args <- list(...)
+    print(args)
+#     print(as.list(match.call(expand.dots = FALSE)))
+    mean.args <- c("trim", "na.rm")
+    sample.args <- c("size", "replace", "prob")
+    
+# Could use a for loop to go over all args.
+    
+#     cat("arguments in mean()", mean.args, "\n")
+    
+    args.names <- names(args)
+    
+#     cat("argument names in ...", args.names, "\n")
+    
+    m.args <- args.names[args.names %in% mean.args]
+    s.args <- args.names[args.names %in% sample.args]
+    
+#     cat("args names that will be used in mean", m.args, "\n")
+    
+    print(args[["m.args"]])
+    m <- list(trim = 0, na.rm = args[[m.args]])
+    s <- list(args[[s.args]], replace = FALSE, prob = NULL)
+    
+#     print(m)
+#     print("args that will be used in mean", m, "\n")
+    
+    fancy.mean <- mean(vec, trim = 0, na.rm = args[[m.args]])
+    fancy.sample <- sample(vec, size = args[[s.args]])
+    return(list(fancy.mean,fancy.sample))
+}
 
+fancy.mean(c(1:5, NA, NA, 234,123,4,123,41,234),
+           na.rm = T, size = 1)
 
+sample.ci <- function(x, digits = 2) {
+    mu <- mean(x)
+    variance <- var(x)
+    size <- length(x)
+    t.stat <- qt(p = 0.025, df = size - 1, lower.tail = FALSE)
+    
+    lb <- mu - t.stat * sqrt(variance/size)
+    ub <- mu + t.stat * sqrt(variance/size)
+    
+    return(round(c(lower = lb, upper = ub), digits))
+    
+    # Think carefully whether this output can be put
+    # inside of a data.frame
+}
+
+aggregate(Wr.Hnd~Smoke, data = data, FUN = sample.ci)
 
 set.seed(20200306)
 N <- 200
 height <- round(rnorm(n = N, mean = 180, sd = 10)) # in centimeter
 weight <- round(rnorm(n = N, mean = 80, sd = 10)) # in kilograms
 age <- round(rnorm(n = N, mean = 50, sd = 10))
-treatment <- sample(c(TRUE, FALSE), size = N, replace = T, prob = c(0.3,0.7))
-HF <- sample(c(TRUE, FALSE), size = N, replace = T, prob = c(0.1,0.9))
+treatment <- sample(c(TRUE, FALSE), size = N,
+                    replace = T, prob = c(0.3,0.7))
+HF <- sample(c(TRUE, FALSE), size = N, replace = T,
+             prob = c(0.1,0.9))
 
 fake <- data.frame(height, weight, age, treatment, HF)
 head(fake)
 
+names(fake)
+fake$BMI <- fake$weight/(fake$height/100)^2
+head(fake)
 
+cut.pts <- c(-Inf, 18.5, 25, 30, Inf)
+labs <- c("Underweight", "Normal weight", "Overweight", "Obesity")
+fake$BMI.cat <- cut(fake$BMI, breaks = cut.pts, labels = labs, right = F)
+head(fake)
 
+# aggregate()
+aggregate(BMI~BMI.cat, data = fake, FUN = mean)
 
+# split() and lapply()
+BMI.grp <- split(fake$BMI, f = fake$BMI.cat)
+lapply(BMI.grp, FUN = mean)
 
 # Trick:
 FALSE+TRUE+TRUE
 
+aggregate(HF~BMI.cat+treatment, data = fake, FUN = sum)
 
+tab2by2 <- function(data, treatment, outcome){
+    sub <- data[, c(treatment, outcome)]
+    return(table(sub))
+}
 
+tab2by2(fake, treatment = "treatment", outcome = "HF")
 
+tab2by2.pro <- function(data, treatment, outcome, treatment.threshold, outcome.threshold){
+    tx <- data[, treatment]
+    rx <- data[, outcome]
+    
+    if (length(table(tx))>2) {
+        if (missing(treatment.threshold)) {
+            stop("Non-binary treatment. Please provide a threshold.")
+        } else {
+            binary.treatment <- ifelse(tx<=treatment.threshold,
+                                       yes = paste("<=", treatment.threshold),
+                                       no = paste(">", treatment.threshold))
+        }
+    } else {
+        binary.treatment <- tx
+    }
+    
+    if (length(table(rx))>2) {
+        if (missing(outcome.threshold)) {
+            stop("Non-binary outcome. Please provide a threshold.")
+        } else {
+            binary.outcome <- ifelse(rx<=outcome.threshold,
+                                     yes = paste("<=", outcome.threshold),
+                                     no = paste(">", outcome.threshold))
+        }
+    } else {
+        binary.outcome <- rx
+    }
+    
+    
+    return(table(treatment = binary.treatment, outcome = binary.outcome))
+}
 
+tab2by2.pro(fake, treatment = "age", outcome = "BMI")
 
+tab2by2.pro(fake, treatment = "age", outcome = "BMI", treatment.threshold = 50)
+
+tab2by2.pro(fake, treatment = "age", outcome = "BMI", treatment.threshold = 50, outcome.threshold = 20)
+
+tab2by2.pro(fake, treatment = "age", outcome = "HF")
+
+# HF is binary, so it is OK if "outcome.threshold" is missing.
+tab2by2.pro(fake, treatment = "age", outcome = "HF", treatment.threshold = 50)
 
 
